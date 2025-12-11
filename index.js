fd9d4b44559950e90c009578f1f6ee9f5047b374
Neeraj Bhatt <neeraj.bhatt099@gmail.com>
2025-12-11 17:10:36 +0530

    401 error

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ app.use(cors({
     // origin: ['http://localhost:5173'],
     credentials: true,
 
-    // allowedHeaders: ["Content-Type", "Authorization"],
-    // methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
 }));
 
 
